a6b7bf29377add98791ef2ee6d3534e9bfd50825
tobibrosch <131682542+tobibrosch@users.noreply.github.com>
2023-04-26 11:35:17 +0200

    Final Version

--- Blatt02/brosch_Blatt02_1.py
+++ Blatt02/brosch_Blatt02_1.py
@@ -1,8 +1,11 @@
 import random
-n=random.randint(1,100)
+n = random.randint(1,100)
 x = float(input("Welche Zahl tippst du?"))
 while x!=n:
-    if x<n:
+    if x%1!=0:
+        print ("Die Zahl ist keine ganze Zahl zwischen 1 und 100!")
+        x = float(input("Möchtest du eine andere Zahl eingeben?"))
+    elif x<n:
         print("Leider daneben, deine Zahl ist kleiner gewesen. Rate erneut.")
         x = float(input("Welche Zahl tippst du?"))
     elif x>n:
