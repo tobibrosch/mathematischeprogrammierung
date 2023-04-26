3bbcbfd8beb87815cde6062e9f3485cfb64b235c
tobibrosch <131682542+tobibrosch@users.noreply.github.com>
2023-04-26 14:47:24 +0200

    Finale Version

--- Blatt02/brosch_Blatt02_3.py
+++ Blatt02/brosch_Blatt02_3.py
@@ -1,9 +1,8 @@
-data = [9,0,7,4,5,2,3,8,1,3,6,5]
-data_sort=[]
-while len(data)>0:
-    data_sort.append(min(data))
-    data.remove(min(data))
-    print(data_sort)
-    
+d1 = [9,0,7,4,5,2,3,8,1,3,6,5]
+d2 = [9,0,7,4,5,2,3,8,1,3,6,5]
 
-
+for i in range(len(d1)):
+    d1[d2.index(min(d2))+i], d1[i] = d1[i], d1[d2.index(min(d2))+i]
+    d2[d2.index(min(d2))], d2[0] = d2[0], d2[d2.index(min(d2))]
+    d2.remove(d2[0])
+print(d1)
