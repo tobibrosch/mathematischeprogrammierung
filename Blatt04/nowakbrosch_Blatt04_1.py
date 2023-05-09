b389f5e1eaff57cb9d266a271aae9a2d3e86dc1e
tobibrosch <131682542+tobibrosch@users.noreply.github.com>
2023-05-09 14:30:29 +0200

    Blatt04

---
+++ Blatt04/nowakbrosch_Blatt04_1.py
@@ -0,0 +1,60 @@
+v = (3,4,-1)
+from math import sqrt, acos
+def vnorm(v):
+    """ 
+    Euklidische Norm
+    ---------------------
+    Berechnung der Euklideschen Norm eines n-Dimensionalen Vektors in R
+    ---------------------
+    Parameter:
+        v = Eingabe eines tupels mit größe n
+        sum = 0 ist die Summe der Diskriminate
+    ---------------------
+    For Schleife Berechnet die Summe der Quadrate der Einträge unter der Diskriminate
+
+    sqrt(sum) berechnet die Quadratwurzel der berechneten Summe "sum" und gibt dies als Wert der Funktion zurück
+    """
+    sum=0
+    for i in range(len(v)):
+        sum = sum + v[i]**2
+    return sqrt(sum)
+
+print(vnorm(v))
+
+def vsum(*vectors):
+    """
+    Addition Vektoren
+    -------------------
+    Summiert endlich viele dreidimensionale Vektoren
+    -------------------
+    Parameter: 
+        vectors = Einhabe von einem tupel mit drei Elementen 
+    
+    Gibt ein tupel mit 3 Elementen zurück
+
+    """
+    a=0
+    b=0
+    c=0
+    for i in range(len(vectors)):
+        a = a+ vectors[i][0]
+    for i in range(len(vectors)):
+        b = b+ vectors[i][1]
+    for i in range(len(vectors)):
+        c = c+ vectors[i][2]
+    return (a,b,c)
+
+#print(vnorm(vsum((1,2,3),(1,2,3),(2,3,-2))))
+
+def vdotproduct(v1,v2):
+    sum=0
+    if len(v1)==len(v2):
+        for i in range(len(v1)):
+            sum= sum + v1[i]*v2[i]
+        return sum 
+    return "Die Vektoren müssen gleicher Dimension sein!"
+#print(vdotproduct((1,2,3),(1,2,3)))
+
+def vangle(v1,v2):
+    return acos(vdotproduct(v1,v2)/(vnorm(v1)*vnorm(v2)))
+print(vangle((0,0,1),(1,0,0)))
