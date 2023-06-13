a2cc220b02b542bfe11f774a0e2ee9c24bc770c6
tobibrosch <131682542+tobibrosch@users.noreply.github.com>
2023-06-13 15:43:27 +0200

    kürzer

--- breakout/breakout_vs2.py
+++ breakout/breakout_vs2.py
@@ -1,9 +1,10 @@
 import pygame
-
+import random
+from math import sqrt
 def moving_rect(dt):
-    global x_speed, y_speed, y_speed_rect2,rectangle_list
-    rect_1.x += x_speed * dt
-    rect_1.y += y_speed * dt
+    global x_speed, y_speed, y_speed_rect2,rectangle_list,dx,dy
+    rect_1.x += x_speed * dt * dx
+    rect_1.y += y_speed * dt * dy
     rect_2.y += y_speed_rect2 * dt
     pygame.draw.rect(screen, "red", rect_1)
     pygame.draw.rect(screen, "white", rect_2)
@@ -13,13 +14,14 @@ def moving_rect(dt):
         rect_1.x, rect_1.y = 400, 400
     if rect_1.left < 0 or rect_1.right > 800:
         x_speed *= -1
-    if rect_2.top < 0 or rect_2.bottom > 800:
-        y_speed_rect2 *= -1
     hitbox = 10
     for rect in rectangle_list:
         if rect_1.colliderect(rect):
             if rect!=rect_2:
                 rectangle_list.remove(rect)  # Entferne das Rechteck aus der Liste
+            if rect==rect_2:
+                dy = random.randint(5,10)/10
+                dx = round(sqrt(1-dy**2),4)
             if abs(rect_1.bottom - rect.top) < hitbox and y_speed > 0:
                 y_speed *= -1
             if abs(rect_1.top - rect.bottom) < hitbox and y_speed < 0:
@@ -41,10 +43,12 @@ rect_2 = pygame.Rect(200, 700, 100, 10)
 rectangle_list = [pygame.Rect(i*100+5,j*25,90,20) for i in range(8) for j in range(8)]
 rectangle_list.append(rect_2)
 #Geschwindigkeiten
-x_speed = 300
+x_speed = 400
 y_speed = 400
 y_speed_rect2 = 0
 x_speed_rect2 = 0
+dx =1
+dy =1
 while running:
     # Beende durch X drücken oder q
     pressed_keys = pygame.key.get_pressed()
